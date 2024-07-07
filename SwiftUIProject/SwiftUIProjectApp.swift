04b3ce0a170da035a1b32d7607d973083fb60504
julianMarzabal <julianmarzabal@gmail.com>
2024-07-07 09:55:15 -0300

    Initial Commit

---
+++ SwiftUIProject/SwiftUIProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIProjectApp.swift
+//  SwiftUIProject
+//
+//  Created by Julian Marzabal on 07/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
